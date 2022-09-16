528e5fce87587fcc7b620e1efc8954995046c3d6
knyeow <ege.yasar37@gmail.com>
2022-09-16 04:00:41 +0300

    LaserBeta

--- MagaraJam#5/Assets/Scripts/Player/Laser.cs
+++ MagaraJam#5/Assets/Scripts/Player/Laser.cs
@@ -6,6 +6,7 @@ public class Laser : MonoBehaviour
 {
     [SerializeField] private float laserPower;
     [SerializeField] private Transform laserPoint;
+    [SerializeField] private TrailRenderer tr;
     private Rigidbody2D rb;
 
     private float lifeTimeTimer=3;
@@ -13,12 +14,17 @@ public class Laser : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        tr = GetComponent<TrailRenderer>();
     }
 
     void Update()
     {
         if (lifeTimeTimer > 3)
+        {
+            tr.enabled = false;
+            tr.Clear();
             transform.position = laserPoint.position;
+        }
 
 
         lifeTimeTimer += Time.deltaTime;
@@ -27,9 +33,17 @@ public class Laser : MonoBehaviour
    
     public void setDirection(Vector2 direction)
     {
-        if(lifeTimeTimer >3)
-        rb.velocity = direction * laserPower*Time.fixedDeltaTime;
-        lifeTimeTimer = 0;
+        if (lifeTimeTimer > 3.5)
+        { 
+            lifeTimeTimer = 0;
+            tr.enabled = true;
+            rb.velocity = direction * laserPower * Time.fixedDeltaTime;
+            
+        }
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        rb.velocity = Vector2.zero;
+    }
 }
